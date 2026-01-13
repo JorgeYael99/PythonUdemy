e6093b4800ed9b03b19187970cf2fd794e0bd4a2
JorgeYael99 <jorg.yael99@gmail.com>
2026-01-13 12:35:37 -0600

    edición de multiplicacion_cadenas

--- cadenas/multiplicacion_cadenas.py
+++ cadenas/multiplicacion_cadenas.py
@@ -8,9 +8,12 @@ print(linea)
 # Salida: ====================
 
 #2. Identación simple
-nivel = 2
+nivel = 1
 sangria = "     "*nivel
 print(sangria+"definición_bloque_codigo:")
 
 # 3 Patrones simples
 print("1"*5) # "11111" (Texto, no numero)
+
+# 4 Error, no se puede multiplicar por un float
+#print("hola"*2.1)
